6c975ef6451fa7ac3a183fe3898611023c752af1
bmonsalves <bdo.monsalves@gmail.com>
2014-03-27 16:15:47 -0300

    carga hola mundo

---
+++ src/com/example/prueba2/MyActivity.java
@@ -0,0 +1,42 @@
+package com.example.prueba2;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MyActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    private TextView texto;
+    private Button diHolabt;
+    private Button restablecerBt;
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        texto = (TextView) findViewById(R.id.texto);
+        diHolabt = (Button) findViewById(R.id.decirHolaBt);
+        restablecerBt = (Button) findViewById(R.id.restablecerBt);
+
+        diHolabt.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                texto.setText("Hola Mundo");
+            }
+        });
+
+        restablecerBt.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                texto.setText("");
+
+            }
+        });
+    }
+}
